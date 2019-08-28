2690011c1b2529b8e69c994c95df4a5bcb23ac44
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-08-28 22:30:25 +0530

    Add login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,45 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class LoginForm extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    final appTitle = 'Form Validation Demo';
-
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(appTitle),
-        ),
-        body: MyCustomForm(),
-      ),
-    );
-  }
+  _LoginFormState createState() => _LoginFormState();
 }
 
-// Create a Form widget.
-class MyCustomForm extends StatefulWidget {
-  @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
-}
-
-// Create a corresponding State class.
-// This class holds data related to the form.
-class MyCustomFormState extends State<MyCustomForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<MyCustomFormState>.
+class _LoginFormState extends State<LoginForm> {
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
       child: Column(
@@ -49,6 +19,14 @@ class MyCustomFormState extends State<MyCustomForm> {
             validator: (value) {
               if (value.isEmpty) {
                 return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text2';
               }
               return null;
             },
